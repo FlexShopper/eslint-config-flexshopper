1b4cbdeff798f56a2d5545bf47073b062ccbdff3
Italo A. Casas <me@italoacasas.com>
2016-01-24 09:18:05 -0500

    adding protractor env

--- angular.js
+++ angular.js
@@ -1,7 +1,11 @@
 module.exports = {
     'extends': 'flexshopper/index.js',
+    'env': {
+        'protractor': true
+    },
     'rules': {
         'strict': [2, 'never']
     },
-    'parser': 'babel-eslint'
+    'parser': 'babel-eslint',
+
 };
