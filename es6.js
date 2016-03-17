af69ef6365930d38e94603cd1d335fbcc88d3d82
Italo A. Casas <me@italoacasas.com>
2016-03-17 11:57:30 -0400

    removing babel parser

--- es6.js
+++ es6.js
@@ -1,17 +1,18 @@
 module.exports = {
     'extends': 'flexshopper/index.js',
     'env': {
-        'es6': true
+        'es6': true,
+        'node': true,
+        'mongo': true,
+        'mocha': true,
+        'commonjs': true
+    },
+    'parserOption': {
+        'ecmaVersion': 6
     },
     'rules': {
         'no-var': 2,
-        'strict': [
-            2,
-            'safe'
-        ],
-    },
-    'parser': 'babel-eslint',
-    'ecmaFeatures': {
-        'blockBindings': true
+        'strict': [2, 'global'],
+        'keyword-spacing': 2
     }
 };
