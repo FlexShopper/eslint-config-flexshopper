ca101852e140d36003f7e8e2a4b819f9b8380508
Geyser Zamora <geyser.zamora@flexshopper.com>
2017-06-29 15:23:34 -0400

    using airbnb eslint

--- angular.js
+++ angular.js
@@ -1,21 +1,74 @@
 'use strict';
 
 module.exports = {
-    'extends': './index.js',
-    'env': {
-        'protractor': true,
-        'jasmine': true,
-        'commonjs': true
+    extends: 'eslint:recommended',
+    parser: 'babel-eslint',
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        mocha: true,
+        mongo: true,
+        protractor: true,
+        jasmine: true,
+        commonjs: true,
     },
-    'rules': {
-        'strict': ['off']
+    parserOptions: {
+        ecmaVersion: 6,
+        ecmaFeatures: {
+            globalReturn: true,
+        },
     },
-    'parser': 'babel-eslint',
-    'parserOptions': {
-        'ecmaVersion': 6
+    rules: {
+        strict: ['off'],
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'max-len': ['error', 120],
+        camelcase: ['error'],
+        eqeqeq: ['error', 'always'],
+        'new-cap': ['error'],
+        'no-bitwise': ['warn'],
+        'no-caller': ['error'],
+        'no-cond-assign': ['error', 'except-parens'],
+        'no-debugger': ['error'],
+        'space-before-function-paren': ['error'],
+        'no-empty': ['error'],
+        'no-eval': ['error'],
+        'no-extend-native': ['error'],
+        'no-irregular-whitespace': ['error'],
+        'no-iterator': ['error'],
+        'no-multi-str': ['error'],
+        'no-new': ['error'],
+        'no-proto': ['error'],
+        'no-script-url': ['error'],
+        'no-sequences': ['error'],
+        'no-undef': ['error'],
+        'no-unused-vars': ['error', { vars: 'all', varsIgnorePattern: '[$rootScope]' }],
+        'no-with': ['error'],
+        'no-inner-declarations': ['warn'],
+        'vars-on-top': ['warn'],
+        'object-curly-spacing': ['error', 'always'],
+        'valid-typeof': ['error'],
+        'keyword-spacing': ['error', { before: true, after: true }],
+        'eol-last': ['error'],
+        'no-console': ['warn'],
     },
-    'globals': {
-        'spyOn': false,
-        'angular': false
-    }
+    globals: {
+        spyOn: false,
+        angular: false,
+        define: false,
+        require: false,
+        exports: false,
+        module: false,
+        describe: false,
+        before: false,
+        beforeEach: false,
+        after: false,
+        afterEach: false,
+        it: false,
+        inject: false,
+        expect: false,
+    },
 };
